85df9a2ee5f2a4245ccd1f3cd6f9b33ee6693126
Travis CI User <travis@example.org>
2018-03-31 20:11:24 +0000

    Update documentation

---
+++ implementors/core/marker/trait.Send.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["lz4_sys"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Send.html\" title=\"trait core::marker::Send\">Send</a> for <a class=\"struct\" href=\"lz4_sys/struct.LZ4FCompressionContext.html\" title=\"struct lz4_sys::LZ4FCompressionContext\">LZ4FCompressionContext</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Send.html\" title=\"trait core::marker::Send\">Send</a> for <a class=\"struct\" href=\"lz4_sys/struct.LZ4FDecompressionContext.html\" title=\"struct lz4_sys::LZ4FDecompressionContext\">LZ4FDecompressionContext</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
